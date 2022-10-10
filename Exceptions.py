7601d64e9df013022d131e3f952f84e393062b42
DanDDXuanX <42824625+DanDDXuanX@users.noreply.github.com>
2022-10-10 22:22:21 +0800

    改了改exception

--- Exceptions.py
+++ Exceptions.py
@@ -3,11 +3,29 @@
 # 异常定义
 
 # 解析器错误
-class ParserError(Exception):
-    def __init__(self,*description):
-        self.description = ' '.join(map(str,description))
+
+# 回声工坊核心的异常基类
+class RplGenError(Exception):
+    def __init__(self, *description):
+        self.errortype = ''
+        self.description = self.errortype + ' '.join(map(str,description))
     def __str__(self):
         return self.description
+
+class ArgumentError(RplGenError):
+    def __init__(self, *description):
+        super().__init__(*description)
+        self.errortype = "\x1B[31m[ArgumentError]:\x1B[0m "
+class ParserError(RplGenError):
+    def __init__(self,*description):
+        super().__init__(*description=)
+    def __str__(self):
+        return self.description
+
+class ParserError(RplGenError):
+    def __init__(self, *description):
+        super().__init__(*description)
+        self.errortype = "\x1B[31m[ParserError]:\x1B[0m "
 
 # 媒体定义错误
 class MediaError(ParserError):
