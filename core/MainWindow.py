9cf3219352dee7f937abd677f0767f798fbd720b
DanDDXuanX <2602694711@qq.com>
2023-03-23 01:36:10 +0800

    alpha 1.22.13
    实现了可折叠的资源管理器

--- core/MainWindow.py
+++ core/MainWindow.py
@@ -200,7 +200,7 @@ class ProjectView(ttk.Frame):
         self.file_manager  = FileManager(master=self, screenzoom=self.sz)
         self.page_notebook = PageNotes(master=self, screenzoom=self.sz)
         self.page_view     = RGLPage(master=self, screenzoom=self.sz, rgl = RplGenLog(file_input=r"./toy/LogFile.rgl"))
-        # self.page_view = MDFPage(master=self, screenzoom=self.sz ,mdf=MediaDef(file_input=r"E:\Data\20220419_星尘的研究\project\Research-of-Stardust\medef.txt"),media_type='Audio')
+        # elf.page_view = MDFPage(master=self, screenzoom=self.sz ,mdf=MediaDef(file_input=r"E:\Data\20220419_星尘的研究\project\Research-of-Stardust\medef.txt"),media_type='Animation')
         # self.page_view = MDFPage(master=self, screenzoom=self.sz ,mdf=MediaDef(file_input="./toy/MediaObject.txt"),media_type='Bubble')
         # 摆放子元件
         self.update_item()
@@ -245,11 +245,12 @@ class FileManager(ttk.Frame):
             buttons.pack(expand=True,fill='x',side='left',anchor='se',padx=0,pady=0)
         self.project_title.pack(fill='x',side='top',padx=0,pady=0,ipadx=0,ipady=0)
         # 文件内容
-        self.project_contene = ttk.Frame(master=self,borderwidth=0,bootstyle='light')
+        self.project_contene = ScrolledFrame(master=self,borderwidth=0,bootstyle='light',autohide=True)
+        self.project_contene.vscroll.config(bootstyle='warning-round')
         self.items = {
-            'mediadef'  : ttk.Button(master=self.project_contene,text='媒体库',bootstyle='success-link'),
-            'chartab'   : ttk.Button(master=self.project_contene,text='角色配置',bootstyle='success-link'),
-            'rplgenlog' : ttk.Button(master=self.project_contene,text='剧本',bootstyle='success-link'),
+            'mediadef'  : MDFCollapsing(master=self.project_contene,screenzoom=self.sz,content=MediaDef()),
+            'chartab'   : CTBCollapsing(master=self.project_contene,screenzoom=self.sz,content=CharTable(file_input = './toy/CharactorTable.tsv')),
+            'rplgenlog' : RGLCollapsing(master=self.project_contene,screenzoom=self.sz,content=RplGenLog()),
         }
         # 放置
         self.update_item()
@@ -257,7 +258,76 @@ class FileManager(ttk.Frame):
     def update_item(self):
         for idx,key in enumerate(self.items):
             fileitem:ttk.Button = self.items[key]
-            fileitem.pack(fill='x',pady=3,side='top')
+            fileitem.pack(fill='x',pady=0,side='top')
+# 项目视图-可折叠类容器
+class FileCollapsing(ttk.Frame):
+    def __init__(self,master,screenzoom:float,fileclass:str,content):
+        self.sz = screenzoom
+        SZ_5 = int(self.sz * 5)
+        SZ_2 = int(self.sz * 2)
+        super().__init__(master=master,borderwidth=0)
+        self.class_text = {'mediadef':'媒体库','chartab':'角色配置','rplgenlog':'剧本文件'}[fileclass]
+        self.collapbutton = ttk.Button(master=self,text=self.class_text,command=self.update_toggle,bootstyle='warning')
+        self.content_frame = ttk.Frame(master=self)
+        self.button_padding = (SZ_5,SZ_2,SZ_5,SZ_2)
+        self.items = {}
+        # self.update_item()
+    def update_item(self):
+        for idx in self.items:
+            this_button = self.items[idx]
+            this_button.pack(fill='x',side='top')
+        self.collapbutton.pack(fill='x',side='top')
+        self.expand:bool = False
+        self.update_toggle()
+    def update_toggle(self):
+        if self.expand:
+            self.content_frame.pack_forget()
+            self.expand:bool = False
+        else:
+            self.content_frame.pack(fill='x',side='top')
+            self.expand:bool = True
+class MDFCollapsing(FileCollapsing):
+    def __init__(self, master, screenzoom: float, content):
+        super().__init__(master, screenzoom, 'mediadef', content)
+        self.items = {
+            'Pos'       :   ttk.Button(master=self.content_frame,text='位置 (Pos)',bootstyle='light',padding=self.button_padding,compound='left'),
+            'Animation' :   ttk.Button(master=self.content_frame,text='立绘 (Animation)',bootstyle='light',padding=self.button_padding,compound='left'),
+            'Bubble'    :   ttk.Button(master=self.content_frame,text='气泡 (Bubble)',bootstyle='light',padding=self.button_padding,compound='left'),
+            'Background':   ttk.Button(master=self.content_frame,text='背景 (Background)',bootstyle='light',padding=self.button_padding,compound='left'),
+            'Audio'     :   ttk.Button(master=self.content_frame,text='音频 (Audio)',bootstyle='light',padding=self.button_padding,compound='left'),
+        }
+        self.update_item()
+class CTBCollapsing(FileCollapsing):
+    def __init__(self, master, screenzoom: float, content:CharTable):
+        super().__init__(master, screenzoom, 'chartab', content)
+        SZ_10 = int(self.sz * 10)
+        SZ_5  = int(self.sz * 5 )
+        SZ_3  = int(self.sz * 3 )
+        SZ_1  = int(self.sz * 1 )
+        # 新建按钮
+        self.add_button = ttk.Button(master=self.collapbutton,text='新增+',bootstyle='warning',padding=0)
+        self.add_button.pack(side='right',padx=SZ_10,pady=SZ_5,ipady=SZ_1,ipadx=SZ_3)
+        self.table = content.export()
+        self.items = {}
+        for name in self.table['Name'].unique():
+            self.items[name] = ttk.Button(master=self.content_frame,text=name,bootstyle='light',padding=self.button_padding,compound='left')
+        self.update_item()
+class RGLCollapsing(FileCollapsing):
+    def __init__(self, master, screenzoom: float, content:RplGenLog):
+        super().__init__(master, screenzoom, 'rplgenlog', content)
+        SZ_10 = int(self.sz * 10)
+        SZ_5  = int(self.sz * 5 )
+        SZ_3  = int(self.sz * 3 )
+        SZ_1  = int(self.sz * 1 )
+        # 新建按钮
+        self.add_button = ttk.Button(master=self.collapbutton,text='新增+',bootstyle='warning',padding=0)
+        self.add_button.pack(side='right',padx=SZ_10,pady=SZ_5,ipady=SZ_1,ipadx=SZ_3)
+        self.items = {
+            '1'     :   ttk.Button(master=self.content_frame,text='第1集',bootstyle='light',padding=self.button_padding,compound='left'),
+            '2'     :   ttk.Button(master=self.content_frame,text='第2集',bootstyle='light',padding=self.button_padding,compound='left'),
+            '3'     :   ttk.Button(master=self.content_frame,text='第3集',bootstyle='light',padding=self.button_padding,compound='left'),
+        }
+        self.update_item()
 # 项目视图-页面标签
 class PageNotes(ttk.Frame):
     """
